e9b58998f557fc8b2391cf857f91673d2a58000f
DShaulov <shaulovl55@gmail.com>
2022-05-08 23:07:04 +0300

    Added Rank

---
+++ AP2-Chat-Dotnet-Rank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AP2_Chat_Dotnet_Rank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
